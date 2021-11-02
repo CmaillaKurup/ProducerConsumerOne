ad906bbc6ef8c9959809e8c10dab02bc0a67cdcc
CmaillaKurup <cami670v@zbc.dk>
2021-11-02 13:34:08 +0100

    produce up to 5 items and consume while posible

--- ProducerConsumerOne/Program.cs
+++ ProducerConsumerOne/Program.cs
@@ -1,12 +1,57 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace ProducerConsumerOne
 {
+    //this code produce up to 5 items and consume while posible
     class Program
     {
+        private static int _itemsHolder;
+        static int item = 0;
+        static Random _random = new Random();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program pg = new Program();
+
+            Thread producer = new Thread(new ThreadStart(pg.Producer));
+            Thread consumer = new Thread(new ThreadStart(pg.Consumer));
+            
+            producer.Start();
+            consumer.Start();
+        }
+        public void Producer()
+        {
+            while (true)
+            {
+                if (_itemsHolder < 5)
+                {
+                    _itemsHolder ++;
+                    Console.WriteLine(_itemsHolder + " producer");
+                }
+                else
+                {
+                    Console.WriteLine("Producer is wating");
+                }
+                Thread.Sleep(_random.Next(200, 1000));
+            }
+        }
+        public void Consumer()
+        {
+            while (true)
+            {
+                if (_itemsHolder > 0)
+                {
+                    _itemsHolder --;
+                    Console.WriteLine(_itemsHolder + " consumer");
+                }
+                else
+                {
+                    Console.WriteLine("Consumer is wating");
+                }
+                Thread.Sleep(_random.Next(200, 1000));
+            }
         }
     }
 }
